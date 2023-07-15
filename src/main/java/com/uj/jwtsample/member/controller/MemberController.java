d9d42f7c6698ec9ab28a85dc5c837d55985ba9f4
kimuijin <dmlwls2435@naver.com>
2023-07-15 23:17:27 +0900

    feat.SpringSecurity 설정

--- src/main/java/com/uj/jwtsample/member/controller/MemberController.java
+++ src/main/java/com/uj/jwtsample/member/controller/MemberController.java
@@ -1,5 +1,22 @@
 package com.uj.jwtsample.member.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/member")
 public class MemberController {
 
+  @PostMapping("/regist")
+  public void registMember() {
+    System.out.println("test");
+  }
+
+  @GetMapping("/regist")
+  public void getMember() {
+    System.out.println("test");
+  }
 }
